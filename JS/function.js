9c79f375e670d12e996ed57a8a55984a0ed17e5f
Adro45 <123975565+Adro45@users.noreply.github.com>
2023-01-30 15:02:16 -0300

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,13 @@
+document.getElementById("obj1").style.display = "none"
+    
+
+document.getElementById('boton').onclick = function () {var x = document.getElementById("obj1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    }
+
+
+  
